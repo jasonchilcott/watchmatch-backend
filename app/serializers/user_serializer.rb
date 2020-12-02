18322cb7d0efa92944a2e09d4b0067625aef608b
jasonchilcott <jasonchilcottcode@gmail.com>
2020-12-02 02:52:02 -0500

    added social to serailizer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :avatar_url, :detailed_bio, :one_line, :password
+  attributes :id, :username, :avatar_url, :detailed_bio, :one_line, :twitter, :instagram
   has_many :ratings
   has_many :movies, through: :ratings
   
