6ab61476a41a55f59e2b6d1036ab90ed55fe7ac4
jasonchilcott <jasonchilcottcode@gmail.com>
2021-03-19 18:07:51 -0400

    commented out EventedFileUpdateChecker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #whitelists heroku
   config.hosts << "watchmatch-api.herokuapp.com"
