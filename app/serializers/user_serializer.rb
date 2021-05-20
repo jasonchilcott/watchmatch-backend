90e316835325557c9bdaa5c79d4fe7e77388e223
jasonchilcott <jasonchilcottcode@gmail.com>
2021-05-20 18:17:26 -0400

    fixed null with hash error

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
+  require_relative '../modules/null_attributes_remover'
+
   include NullAttributesRemover
+
   attributes :id, :username, :avatar_url, :detailed_bio, :one_line, :twitter, :instagram
   has_many :ratings
   has_many :movies, through: :ratings
