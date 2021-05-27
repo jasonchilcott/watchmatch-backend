dca849afa7754879e5857b49d4cf402c80efc53c
jasonchilcott <jasonchilcottcode@gmail.com>
2021-05-27 17:00:54 -0400

    create user error messages in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :ratings
   has_many :movies, through: :ratings
   
-  validates :username, uniqueness: { case_sensitive: false }
-  validates_format_of :username, with: /\A([a-zA-Z0-9]){1}([a-zA-Z0-9\_]){4,15}\z/i
+  validates :username, uniqueness: { case_sensitive: false }, :message=>"Username is already taken"
+  validates_format_of :username, with: /\A([a-zA-Z0-9]){1}([a-zA-Z0-9\_]){4,15}\z/i, :message=>"Username contains invalid characters"
   validates :password, presence: true, on: :create
 end
