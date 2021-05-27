d57788ac2ff168f7225e9b108191f3e9b87b5ff3
jasonchilcott <jasonchilcottcode@gmail.com>
2021-05-27 16:32:25 -0400

    user create error in model?

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :ratings
   has_many :movies, through: :ratings
   
-  validates :username, uniqueness: { case_sensitive: false }, :errors => 'username already taken'
+  validates :username, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /\A([a-zA-Z0-9]){1}([a-zA-Z0-9\_]){4,15}\z/i
   validates :password, presence: true, on: :create
 end
